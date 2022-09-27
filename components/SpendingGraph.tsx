94b4be7c383cfa0e4ad415f8b005339c02affd7f
Pablo Asenjo <pasenjo94@gmail.com>
2022-09-27 17:45:04 +0200

    added display individual values on hover

--- components/SpendingGraph.tsx
+++ components/SpendingGraph.tsx
@@ -17,12 +17,24 @@ export default function SpendingGraph({ data }: { data: DaySpending[] }) {
         >
           {/* if value is max, different color and set height */}
           {day.amount === maxValue ? (
-            <div className="rounded h-32 w-6 bg-primary-cyan"></div>
+            <div className="rounded h-32 w-6 bg-primary-cyan hover:opacity-60 group relative duration-150">
+              <div className="pointer-events-none opacity-0 group-hover:opacity-100 duration-150 absolute z-50 bottom-full">
+                <span className="text-xs px-2 py-1 z-50 border-2 border-slate-300 rounded-lg bg-white">
+                  ${day.amount}
+                </span>
+              </div>
+            </div>
           ) : (
             <div
-              className="rounded w-6 bg-primary-red"
+              className="rounded w-6 bg-primary-red group relative duration-150"
               style={{ height: `${(day.amount / maxValue) * 100}%` }}
-            ></div>
+            >
+              <div className="pointer-events-none opacity-0 group-hover:opacity-100 duration-150 bottom-full z-50 absolute">
+                <span className="text-xs px-2 py-1 z-50 border-2 border-slate-300 rounded-lg bg-white">
+                  ${day.amount}
+                </span>
+              </div>
+            </div>
           )}
           <span className="text-xs opacity-60">{day.day}</span>
         </div>
